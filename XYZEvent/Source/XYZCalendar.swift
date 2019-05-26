fc603c866f3cb90e271f1008eaf167d8b14f4624
Brando <zhangqianbrandy2012@gmail.com>
2019-05-26 14:55:18 +0800

    version 0.0.7

--- XYZEvent/Source/XYZCalendar.swift
+++ XYZEvent/Source/XYZCalendar.swift
@@ -344,3 +344,81 @@ public extension XYZCalendarKit{
 //        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
 //        let date = formatter.date(from: "2019-04-26 19:05:00")
 //
+
+
+
+
+
+//
+////请求日历
+//func requestCalendars() {
+//    // 请求日历事件
+//    eventStore.requestAccess(to: .event, completion: {
+//        granted, error in
+//        if (granted) && (error == nil) {
+//            print("granted \(granted)")
+//            print("error  \(error)")
+//
+//            //获取本地日历（剔除节假日，生日等其他系统日历）
+//            let calendars = self.eventStore.calendars(for: .event).filter({
+//                (calender) -> Bool in
+//                return calender.type == .local || calender.type == .calDAV
+//            })
+//
+//            self.events = []
+//            //获取当前年
+//            let com = Calendar.current.dateComponents([.year], from: Date())
+//            let currentYear = com.year!
+//            print(currentYear)
+//
+//            // 获取所有的事件（前后20年）
+//            for i in -20...20 {
+//                let startDate = self.startOfMonth(year:currentYear+i, month:1)
+//                let endDate = self.endOfMonth(year:currentYear+i, month:12,
+//                                              returnEndTime:true)
+//                print("查询范围 开始:\(startDate) 结束:\(endDate)")
+//
+//                let predicate2 = self.eventStore.predicateForEvents(
+//                    withStart: startDate, end: endDate, calendars: calendars)
+//
+//                if let eV = self.eventStore.events(matching: predicate2) as [EKEvent]!
+//                {
+//                    //将获取到的日历事件添加到集合中
+//                    self.events.append(contentsOf: eV)
+//                }
+//            }
+//
+//            print("事件加载完毕!共\(self.events.count)条数据。")
+//            //重新刷新表格数据
+//            DispatchQueue.main.async {
+//                self.tableView?.reloadData()
+//            }
+//        }
+//    })
+//}
+////指定年月的开始日期
+//func startOfMonth(year: Int, month: Int) -> Date {
+//    let calendar = Calendar.current
+//    var startComps = DateComponents()
+//    startComps.day = 1
+//    startComps.month = month
+//    startComps.year = year
+//    let startDate = calendar.date(from: startComps)!
+//    return startDate
+//}
+//
+////指定年月的结束日期
+//func endOfMonth(year: Int, month: Int, returnEndTime:Bool = false) -> Date {
+//    let calendar = Calendar.current
+//    var components = DateComponents()
+//    components.month = 1
+//    if returnEndTime {
+//        components.second = -1
+//    } else {
+//        components.day = -1
+//    }
+//    let tem = startOfMonth(year: year, month:month)
+//    let endOfYear =  calendar.date(byAdding: components, to: tem)!
+//    return endOfYear
+//}
+//
